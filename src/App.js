92acb04ab493d2eadfa0949b9fc1f9f9452d0425
Israel <93683580+blueishegg@users.noreply.github.com>
2022-02-09 16:05:34 +0000

    added the functionality to toggle the player

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Board from "./components/Board";
 
 function App() {
-  const [squares, setSquares] = useState([null, "X", null, "O", null, null, null, "O", null]);
+  const [squares, setSquares] = useState(Array(9).fill(null));
   const [XTurn, setXTurn] = useState(true);
 
   function toggleXTurn() {
@@ -10,17 +10,25 @@ function App() {
   }
 
   function onClick(index) {
-    // check if the index is null then update `squares` with setSquares to X or O at appropriate index
+    // check if the state is null then update `squares` with setSquares to X or O at appropriate index
+    if (XTurn === true) {
+      let squaresCopy = [...squares];
+      squaresCopy[index] = "X";
+      setSquares(squaresCopy);
+      toggleXTurn();
+    } else {
+      let squaresCopy = [...squares];
+      squaresCopy[index] = "O";
+      setSquares(squaresCopy);
+      toggleXTurn();
+    }
     // depending on the move that was done before (we'll need some sort of history tracking)
-    let squaresCopy = [...squares];
-    squaresCopy[index] = 'X';
-    setSquares(squaresCopy);
   }
 
   return (
     <div className="App">
       <h1>Play our tick tack toe's, woo!</h1>
-      <Board squares={squares} onClick={onClick}/>
+      <Board squares={squares} onClick={onClick} />
       <p>DEBUG: {squares}</p>
     </div>
   );
