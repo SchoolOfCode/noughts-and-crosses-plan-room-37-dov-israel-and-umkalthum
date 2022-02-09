67cc5a1a9e63cc3cf17416bccbd98430a4b90162
Israel <93683580+blueishegg@users.noreply.github.com>
2022-02-09 14:21:18 +0000

    creating the Board component(sort of) and displaying the buttons on the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // game logic here
+import React from "react";
+import Board from "./components/Board";
 
 function App() {
+  const squares = [null, "X", null, "O", null, null, null, null, null];
+
   return (
     <div className="App">
       <h1>Play our tick tack toe's, woo!</h1>
+      <Board squares={squares} />
     </div>
   );
 }
 
 export default App;
- 
--- src/components/Board.js
+++ src/components/Board.js
@@ -6,3 +6,17 @@
   - Render
     - Squares - 1 for each item in board, arranged into a grid
 */
+import React from "react";
+import Square from "./Square";
+
+function Board({ squares, onClick }) {
+  return (
+    <div>
+      {squares.map((square) => {
+        return <Square value={square} />;
+      })}
+    </div>
+  );
+}
+
+export default Board;
